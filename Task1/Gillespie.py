3f8be2d8f71a87d1e0c7761c05d14b5d7ecc4448
yagami <yagami@debian>
2017-05-18 07:12:06 -0400

    Update Gillespie

--- Task1/Gillespie.py
+++ Task1/Gillespie.py
@@ -1,5 +1,9 @@
 import matplotlib.pyplot as pyplot
-from scipy.stats import rv_continuous
+from scipy.stats import *
+import numpy as np
+import math
+import random
+
 print("Doing Analysis for M-M Equation")
 print("Possible Reaction that can take place")
 print("\n")
@@ -9,13 +13,83 @@ print("3. E + P -->  ES , with K3 rate constant")
 print("4. ES --> E + P , with K4 rate constant")
 print("\n")
 print("Here E , S , ES and P are the fundamental species which we need track")
+
 number_of_chemical_species = 4
 number_of_possible_reaction = 4
-time_vector = []
-rate_of_reaction = [2 , 1 , 3 , 4]
-X = [233 , 403 , 133 , 189]
-a_ut = [ X[0]*X[1]*rate_of_reaction[0] , rate_of_reaction[1]*X[1] , rate_of_reaction[2]*X[0]*X[3] , rate_of_reaction[3]*X[2] ]
-a0t = sum(a_ut) 
-tinit = 0
-time_vector.append(tinit)
-tend = 10
+
+r1 = 2
+r2 = 3
+r3 = 4
+r4 = 1
+
+E_current = 401
+S_current = 230
+Es_current = 194
+P_Current =121
+
+time_initial = 0
+
+max_reaction = 10000
+max_time = 1
+
+E_array = np.zeros(max_reaction+1)
+S_array = np.zeros(max_reaction+1)
+P_array = np.zeros(max_reaction+1)
+ES_array = np.zeros(max_reaction+1)
+tt_array = np.zeros(max_reaction+1)
+
+a1 = r1*E_current*S_current
+a2 = r2*Es_current
+a3 = r3*E_current*P_Current
+a4 = r4*P_Current
+
+a0 = a1 + a2 + a3 + a4
+
+r_counter = 0
+
+while r_counter < max_reaction and time_initial < max_time:
+
+	E_array[r_counter] = E_current
+	S_array[r_counter] = S_current
+	P_array[r_counter] = P_Current
+	ES_array[r_counter] = Es_current
+
+	r_counter = r_counter + 1
+
+	rn1 = round(random.uniform(0.1, 1.0), 10)
+	rn2 = round(random.uniform(0.1, 1.0), 10)
+	
+	t_next = (1/a0)*(math.log(1/rn1))
+	tt_array[r_counter]=t_next 
+
+	time_initial = time_initial + t_next
+
+	if rn2 >= 0 and rn2 < a1/a0:
+
+		E_current = E_current - 1
+		S_current = S_current - 1
+		Es_current = Es_current + 1
+
+	elif rn2 >= a1/a0  and rn2 < (a1+a2)/a0:
+
+		Es_current = Es_current - 1
+		E_current = E_current + 1
+		S_current = S_current + 1
+
+	elif rn2 >= (a1+a2)/a0 and rn2 < (a1+a2+a3)/a0:
+
+		E_current = E_current - 1
+		P_Current = P_Current - 1
+		Es_current = Es_current + 1
+
+	elif rn2 >= (a1+a2+a3)/a0 and rn2 < 1:
+
+		Es_current = Es_current - 1
+		E_current = E_current + 1
+		P_Current = P_Current + 1
+
+pyplot.plot(E_array)
+pyplot.plot(S_array)
+pyplot.plot(ES_array)
+pyplot.plot(P_array)
+pyplot.show()
